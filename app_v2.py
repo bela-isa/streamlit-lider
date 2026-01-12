514d6e37a1d833b2b3acd5dffb60ee041cdafdd6
Isa Bela <93232499+bela-isa@users.noreply.github.com>
2026-01-11 21:13:23 -0300

    Update app_v2.py

--- app_v2.py
+++ app_v2.py
@@ -685,7 +685,7 @@ st.markdown(
 <div class="dashboard-header">
   <h1 class="dashboard-title">Análise SEO Grupo Líder</h1>
   <p class="dashboard-subtitle">Monitoramento de performance e análise competitiva</p>
-  <div class="dashboard-meta">  {st.session_state.ui_theme}</div>
+  <div class="dashboard-meta">  </div>
 </div>
 """,
     unsafe_allow_html=True,
